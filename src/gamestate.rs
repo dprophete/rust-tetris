b6309050aec16ab116cc427cff67b0cf9dcf997f
Didier Prophete <didier.prophete@gmail.com>
2024-06-13 10:37:37 -0700

    splitting into files

---
+++ src/gamestate.rs
@@ -0,0 +1,131 @@
+use std::cmp::{max, min};
+
+use ruscii::{drawing::Pencil, spatial::Vec2, terminal::Color};
+
+use crate::tetromino::Tetromino;
+
+const GRID_WIDTH: i32 = 20;
+const GRID_HEIGHT: i32 = 40;
+
+pub struct GameState {
+    pub dimension: Vec2,
+    pub grid: [[Cell; GRID_WIDTH as usize]; GRID_HEIGHT as usize],
+    pub gpos: Vec2,
+    pub pieces: Vec<Piece>,
+    pub current_piece: Option<Piece>,
+}
+
+impl GameState {
+    pub fn new(dim: Vec2) -> Self {
+        Self {
+            dimension: dim,
+            grid: [[Cell::Empty; GRID_WIDTH as usize]; GRID_HEIGHT as usize],
+            gpos: Vec2::xy((dim.x - GRID_WIDTH * 2) / 2, (dim.y - GRID_HEIGHT) / 2),
+            pieces: vec![],
+            current_piece: None,
+        }
+    }
+
+    pub fn place_piece(&mut self, piece: &Piece) {
+        for cell in piece.cells().iter() {
+            let x = piece.pos.x + cell.x;
+            let y = piece.pos.y + cell.y;
+            if x >= 0 && x < GRID_WIDTH && y >= 0 && y < GRID_HEIGHT {
+                self.grid[y as usize][x as usize] = Cell::Tetromino(piece.tetromino);
+            }
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.grid = [[Cell::Empty; GRID_WIDTH as usize]; GRID_HEIGHT as usize];
+
+        // static pieces
+        let pieces = self.pieces.clone();
+        for piece in pieces.iter() {
+            self.place_piece(piece);
+        }
+
+        // moving piece
+        if let Some(piece) = self.current_piece {
+            self.place_piece(&piece);
+        }
+    }
+
+    pub fn upd_gpos(&mut self, delta: Vec2) {
+        self.gpos += delta;
+        self.gpos.x = max(0, self.gpos.x);
+        self.gpos.y = max(0, self.gpos.y);
+        self.gpos.x = min(self.dimension.x - GRID_WIDTH * 2, self.gpos.x);
+        self.gpos.y = min(self.dimension.y - GRID_WIDTH * 2, self.gpos.y);
+    }
+
+    pub fn draw(&mut self, pencil: &mut Pencil) {
+        for (y, row) in self.grid.iter().enumerate() {
+            let y = y as i32;
+            for (x, cell) in row.iter().enumerate() {
+                let x = x as i32 * 2;
+                let pos = Vec2::xy(x + self.gpos.x, y + self.gpos.y);
+                match cell {
+                    Cell::Empty => pencil.set_background(Color::Black).draw_text("..", pos),
+                    Cell::Tetromino(tetromino) => pencil
+                        .set_background(tetromino.color())
+                        .draw_text("  ", pos),
+                };
+            }
+        }
+    }
+
+    pub fn init_with_all_pieces(&mut self) {
+        for (t_nb, t) in [
+            Tetromino::I,
+            Tetromino::J,
+            Tetromino::L,
+            Tetromino::O,
+            Tetromino::S,
+            Tetromino::T,
+            Tetromino::Z,
+        ]
+        .iter()
+        .enumerate()
+        {
+            for rot in 0..=3 {
+                let mut piece = Piece::new(*t);
+                piece.pos = Vec2::xy(rot * 5, t_nb as i32 * 5);
+                piece.rotate(rot);
+                self.pieces.push(piece);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Piece {
+    pub tetromino: Tetromino,
+    pub rot: i32,
+    pub pos: Vec2,
+}
+
+impl Piece {
+    pub fn new(tetromino: Tetromino) -> Self {
+        Self {
+            tetromino,
+            rot: 0,
+            pos: Vec2::zero(),
+        }
+    }
+
+    pub fn rotate(&mut self, delta: i32) -> &Self {
+        self.rot = (self.rot + delta) % 4;
+        self
+    }
+
+    pub fn cells(&self) -> Vec<Vec2> {
+        self.tetromino.cells(self.rot)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Cell {
+    Empty,
+    Tetromino(Tetromino),
+}
---
+++ src/tetromino.rs
@@ -0,0 +1,214 @@
+use ruscii::{spatial::Vec2, terminal::Color};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Tetromino {
+    // X
+    // X
+    // X
+    // X
+    I,
+    //   X
+    //   X
+    // X X
+    J,
+    // X
+    // X
+    // X X
+    L,
+    // X X
+    // X X
+    O,
+    //   X X
+    // X X
+    S,
+    // X X X
+    //   X
+    T,
+    // X X
+    //   X X
+    Z,
+}
+
+impl Tetromino {
+    pub fn color(&self) -> Color {
+        match self {
+            Tetromino::O => Color::Yellow,
+            Tetromino::I => Color::Cyan,
+            Tetromino::S => Color::Green,
+            Tetromino::Z => Color::Red,
+            Tetromino::L => Color::Xterm(208),
+            Tetromino::J => Color::Blue,
+            Tetromino::T => Color::Magenta,
+        }
+    }
+
+    // using: https://strategywiki.org/wiki/File:Tetris_rotation_super.png
+    pub fn cells(&self, mut rot: i32) -> Vec<Vec2> {
+        rot = rot % 4;
+        match self {
+            Tetromino::O => vec![
+                Vec2::xy(1, 0),
+                Vec2::xy(2, 0),
+                Vec2::xy(1, 1),
+                Vec2::xy(2, 1),
+            ],
+
+            Tetromino::I => match rot {
+                0 => vec![
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(3, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(2, 0),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(2, 2),
+                    Vec2::xy(2, 3),
+                ],
+                2 => vec![
+                    Vec2::xy(0, 2),
+                    Vec2::xy(1, 2),
+                    Vec2::xy(2, 2),
+                    Vec2::xy(3, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                    Vec2::xy(1, 3),
+                ],
+            },
+            Tetromino::S => match rot {
+                0 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(2, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(2, 2),
+                ],
+                2 => vec![
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(0, 2),
+                    Vec2::xy(1, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(0, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                ],
+            },
+            Tetromino::Z => match rot {
+                0 => vec![
+                    Vec2::xy(0, 0),
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(2, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(1, 2),
+                ],
+                2 => vec![
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                    Vec2::xy(2, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(0, 2),
+                ],
+            },
+            Tetromino::L => match rot {
+                0 => vec![
+                    Vec2::xy(2, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                    Vec2::xy(2, 2),
+                ],
+                2 => vec![
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(0, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(0, 0),
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                ],
+            },
+            Tetromino::J => match rot {
+                0 => vec![
+                    Vec2::xy(0, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(2, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                ],
+                2 => vec![
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(2, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(0, 2),
+                    Vec2::xy(1, 2),
+                ],
+            },
+            Tetromino::T => match rot {
+                0 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                ],
+                1 => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(1, 2),
+                ],
+                2 => vec![
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(2, 1),
+                    Vec2::xy(2, 2),
+                ],
+                _ => vec![
+                    Vec2::xy(1, 0),
+                    Vec2::xy(0, 1),
+                    Vec2::xy(1, 1),
+                    Vec2::xy(1, 2),
+                ],
+            },
+        }
+    }
+}
